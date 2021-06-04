1e50fa18450eaed1403e24ee0831f1c0a764d718
xyfer17 <nav3044999@gmail.com>
2021-06-04 23:08:47 +0530

    modified update

--- app.py
+++ app.py
@@ -4,13 +4,16 @@ import numpy as np
 from PIL import Image
 import cv2
 import keras
+import mediapipe as mp
 
 # Flask utils
 from flask import Flask, redirect,url_for,request,render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+#run_with_ngrok(app)
 
 path = 'model/mask.h5'
 proto = 'model/deploy.prototxt'
@@ -18,9 +21,33 @@ resNet = 'model/res10_300x300_ssd_iter_140000.caffemodel'
 
 model = keras.models.load_model(path)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
 
 mask_label = {0:'MASK',1:'NO MASK'}
 dist_label = {0:(0,255,0),1:(255,0,0)}
+
+
+def check(X,Y,img):
+
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+                X.append(cx)
+                Y.append(cy)
+
+
+
 
 def model_predict(img,model):
     net  = cv2.dnn.readNetFromCaffe(proto, resNet)
@@ -32,29 +59,48 @@ def model_predict(img,model):
     net.setInput(blob)
     detections = net.forward()
     count = 0
-    res=''
+    X=[]
+    Y=[]
+
+    res='Try Again and capture a bright photo'
 
     for i in range(0, detections.shape[2]):
 
         confidence = detections[0, 0, i, 2]
 
-        if confidence > 0.5:
-            count+1
+        if confidence > 0.6:
+            count+=1
 
 
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             (startX, startY, endX, endY) = box.astype("int")
 
-            text = "{:.2f}%".format(confidence * 100) + 'Count ' + str(count)
+            #text = "{:.2f}%".format(confidence * 100) + 'Count ' + str(count)
             y = startY - 10 if startY - 10 > 10 else startY + 10
+            check(X,Y,image)
             crop = image[startY:endY,startX:endX]
             crop = cv2.resize(crop,(224,224))
             crop = np.reshape(crop,[1,224,224,3])/255.0
             result = model.predict(crop)
-            res = mask_label[result.argmax()]
+            
+
+            
+
+            if np.size(X)==0 and np.size(Y)==0:
+               
+                res = mask_label[result.argmax()]
+
+                
+            else:
+                res =  'Remove your hand from face.'
+                              
+
             cv2.rectangle(image, (startX, startY), (endX, endY),dist_label[result.argmax()], 2)
-            cv2.putText(image,mask_label[result.argmax()], (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+            cv2.putText(image,mask_label[result.argmax()], (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2) 
     
+    if count>1:
+        return "Try Again"
+        
     cv2.imwrite('static/uploads/res.jpg',image)
     return res
 
@@ -86,6 +132,6 @@ def upload():
 if __name__ == '__main__':
 
     
-    app.run(host="192.168.29.186", port=800, debug=False,ssl_context=('cert.pem', 'key.pem'))
+    #app.run(host="192.168.29.186", port=800, debug=False,ssl_context=('cert.pem', 'key.pem'))
 
-    #app.run(debug=True)
+    app.run()
